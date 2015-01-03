8fa9840bfe39311ec89c3946b673d6f8097c5dc3
Christopher Trudeau <trudeau.cj@gmail.com>
2015-01-03 15:40:37 -0800

    Adding sensor data calls

--- spikes/m2x-node/utils.js
+++ spikes/m2x-node/utils.js
@@ -1,5 +1,6 @@
 var request = require('request'),
-  Q = require('q');
+  Q = require('q'),
+  _ = require('underscore');
 
 var appKey = process.env.DIGITAL_LIFE_APP_KEY;
 var userId = process.env.DIGITAL_LIFE_USERID;
@@ -13,7 +14,7 @@ if (!password) return console.log('Please set DIGITAL_LIFE_PASSWORD environment 
 exports.serviceURL = "https://systest.digitallife.att.com:443/penguin/api/";
 
 
-exports.get = function(options) {
+exports.post = function(options) {
   var temp = exports.serviceURL;
   temp = temp + options.API;
 
@@ -39,6 +40,33 @@ exports.get = function(options) {
   return deferred.promise;
 };
 
+
+exports.get = function(options) {
+  var temp = exports.serviceURL;
+  temp = temp + options.API;
+
+  var deferred = Q.defer();
+
+  request({url:temp, headers:options.headers}, function(error, response, body) {
+    // Error with the actual request
+    if (error) {
+      return deferred.reject(error);
+    }
+
+    // Non-200 HTTP response code
+    if (response.statusCode != 200) {
+      return deferred.reject(response);
+    }
+
+    var parsedBody = JSON.parse(body);
+
+    // 200, but Error in token payload
+    if (parsedBody.Error) return deferred.reject({error: parsedBody});
+    deferred.resolve(parsedBody);
+  });
+  return deferred.promise;
+};
+
 exports.getAuth = function(options) {
   options = {
     API : 'authtokens',
@@ -49,13 +77,63 @@ exports.getAuth = function(options) {
       domain : domain
     }
   };
-  exports.get(options)
-    .then(function(data) {
-      console.log(data);
-    })
-    .fail(function(error) {
-      console.log(error);
-    });
+  return exports.post(options);
 };
 
-exports.getAuth();
+exports.getDevices = function(options) {
+  return exports.getAuth()
+  .then(function (data) {
+    var gatewayId = data.content.gateways[0].id;
+    options = {
+      API : gatewayId + '/devices',
+      headers : {
+        Authtoken : data.content.authToken,
+        Requesttoken: data.content.requestToken,
+        Appkey : appKey
+      }
+    };
+    return exports.get(options)
+  });
+};
+
+//thermostat
+//light switch
+//motion detector
+//door lock
+//water sensor
+
+
+exports.getDeviceByName = function(name) {
+  var deferred = Q.defer();
+
+  exports.getDevices()
+  .then(function(devices) {
+    deferred.resolve(_.where(devices.content, {'deviceType':name}));
+  });
+  return deferred.promise;
+};
+
+exports.getThermostat = function() {
+  return exports.getDeviceByName('thermostat')
+};
+
+exports.getWaterSensor = function() {
+  return exports.getDeviceByName('water-sensor')
+};
+
+exports.getMontionSensor = function() {
+  return exports.getDeviceByName('motion-sensor')
+};
+
+exports.getDoorlock = function() {
+  return exports.getDeviceByName('door-lock')
+};
+
+exports.getDoorlock = function() {
+  return exports.getDeviceByName('door-lock')
+};
+
+
+exports.getThermostat();
+
+
